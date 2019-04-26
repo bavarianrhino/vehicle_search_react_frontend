dcdaaa76ddf067aa338093f23911c6e82369a0fb
github username <github email address>
2019-04-26 10:46:44 -0400

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+// import GameLoader from "./containers/GameLoader";
+// import CarCreatorContainer from "./containers/CarCreatorContainer";
+// import GameContainer from "./containers/GameContainer";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Login />
+			<SignUp />
+		</div>
+	);
 }
 
 export default App;
+
+// import React, { Component } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Redirect,
+//   Switch
+// } from "react-router-dom";
+// import Login from "./components/Login";
+// import SignUp from "./components/SignUp";
+// import GameLoader from "./containers/GameLoader";
+// import CarCreatorContainer from "./containers/CarCreatorContainer";
+// import GameContainer from "./containers/GameContainer";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <Switch>
+//           <Route
+//             exact
+//             path="/login"
+//             render={props => {
+//               return !localStorage.getItem("token") ? (
+//                 <Login {...props} />
+//               ) : (
+//                 <Redirect to="/" />
+//               );
+//             }}
+//           />
+//           <Route
+//             exact
+//             path="/signup"
+//             render={props => {
+//               return !localStorage.getItem("token") ? (
+//                 <SignUp {...props} />
+//               ) : (
+//                 <Redirect to="/" />
+//               );
+//             }}
+//           />
+//           <Route
+//             exact
+//             path="/"
+//             render={props => {
+//               return localStorage.getItem("token") ? (
+//                 <GameLoader {...props} />
+//               ) : (
+//                 <Redirect to="/login" />
+//               );
+//             }}
+//           />
+//           <Route
+//             exact
+//             path="/create"
+//             render={props => {
+//               return localStorage.getItem("token") ? (
+//                 <CarCreatorContainer {...props} />
+//               ) : (
+//                 <Redirect to="/login" />
+//               );
+//             }}
+//           />
+//           <Route
+//             exact
+//             path="/game"
+//             render={props => {
+//               return localStorage.getItem("car") ? (
+//                 <GameContainer {...props} />
+//               ) : (
+//                 <Redirect to="/" />
+//               );
+//             }}
+//           />
+//         </Switch>
+//       </Router>
+//     );
+//   }
+// }
+
+// export default App;
