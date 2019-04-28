7388128003c0503b5442dcff5bc6d52cd8ff8570
github username <github email address>
2019-04-28 01:22:56 -0400

    added navbar code structure

---
+++ src/NavBar/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Searcher from './SearcherPage/Searcher';
+import { connect } from 'react-redux';
+
+// import { } from 'semantic-ui-react';
+
+
+class NavBar extends React.Component {
+
+    state = {
+        attr: null
+    }
+
+    funcName = (e) => {
+        console.log(e.target.value)
+    }
+
+    render() {
+        return (
+            <div>
+                {this.funcName}
+                <Searcher />
+            </div>
+        )
+    }
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+        attr: state.attr
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    functionName: (param) => dispatch({ type: 'ACTION_NAME', param })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
