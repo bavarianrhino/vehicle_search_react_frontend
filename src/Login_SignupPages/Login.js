0d5858defc47895ec183658448468c18cffb1b2e
github username <github email address>
2019-04-28 03:50:36 -0400

    added login form from semantic ui react

--- src/Login_SignupPages/Login.js
+++ src/Login_SignupPages/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ProjectComponent from './';
+
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
+
 import { connect } from 'react-redux';
 
-// import { } from 'semantic-ui-react';
-import ProjectComponent from './';
 
 
 class Login extends React.Component {
@@ -19,8 +21,25 @@ class Login extends React.Component {
 	render() {
 		return (
 			<div>
-				{this.funcName}
-				<ProjectComponent />
+				<Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+					<Grid.Column style={{ maxWidth: 450 }}>
+						<Header as='h2' color='teal' textAlign='center'>
+							<Image src='/logo.png' /> Log-in to your account
+						</Header>
+
+						<Form size='large' onSubmit={this.handleSubmit}>
+							<Segment stacked>
+								<Form.Input fluid icon='user' iconPosition='left' autoComplete='off' placeholder='Username' name='username' onChange={this.handleChange} />
+								<Form.Input fluid icon='lock' iconPosition='left' autoComplete='off' placeholder='Password' type='password' name='username' onChange={this.handleChange} />
+								<Button color='teal' fluid size='large' type='submit'>Login</Button>
+							</Segment>
+						</Form>
+
+						<Message>
+							New to us? <a href='#'>Sign Up</a>
+						</Message>
+					</Grid.Column>
+				</Grid>
 			</div>
 		);
 	}
@@ -37,4 +56,4 @@ const mapDispatchToProps = (dispatch) => ({
     functionName: (param) => dispatch({ type: 'ACTION_NAME', param })
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+    export default connect(mapStateToProps, mapDispatchToProps)(Login);
