62028abf84797390104af0b1f26df7c425ea2222
github username <github email address>
2019-04-28 01:25:13 -0400

    added searcher code structure

---
+++ src/SearcherPage/Searcher.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import CarsForSaleSearchForm from './CarsForSaleSearchForm';
+import CarValueSearchForm from './CarValueSearchForm';
+import { connect } from 'react-redux';
+
+// import { } from 'semantic-ui-react';
+
+
+class Searcher extends React.Component {
+
+    state = {
+        attr: null
+    }
+
+    funcName = (e) => {
+        console.log(e.target.value)
+    }
+
+    render() {
+        return (
+			<div>
+				{this.funcName}
+				<CarsForSaleSearchForm />
+                <CarValueSearchForm />
+			</div>
+		);
+    }
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+        attr: state.attr
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    functionName: (param) => dispatch({ type: 'ACTION_NAME', param })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Searcher);
