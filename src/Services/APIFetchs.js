9c4beb8796c06e0a5841d2f8dcc5cdd62286c42c
github username <github email address>
2019-04-29 20:11:30 -0400

    api cleanup

--- src/Services/APIFetchs.js
+++ src/Services/APIFetchs.js
@@ -37,7 +37,7 @@ export const signUpNewUser = (loginData) => {
 // export const getCarValueByVIN = (VINData) => {
 //     console.log(VINData);
 //     let settings = {
-//         'url': 'https://marketcheck-prod.apigee.net/v1/history/1G4HP57228U166894?api_key=vppefiprYFtfGZV0M3vTxaIauROiVoVB',
+//         'url': 'https://marketcheck-prod.apigee.net/v1/history/1G4HP57228U166894?api_key={key}',
 //         'method': 'GET',
 //         'timeout': 0,
 //         'headers': {
@@ -50,9 +50,8 @@ export const signUpNewUser = (loginData) => {
 //     });
 // };
 
-//http://api.marketcheck.com/v1/search?api_key={{api_key}}&vins=19XFB2F5XFE054327&latitude=34.05&longitude=-118.24&radius=100&car_type=used&start=0&rows=10
-// return fetch(`https://marketcheck-prod.apigee.net/v1/search?api_key=${MARKETCHECKAPIKEY}&vins=${VINData}&latitude=33.7872303&longitude=-84.3826917&radius=200&car_type=used&start=0&rows=10`,
-// return fetch('http://api.marketcheck.com/v1/vin/1G4HP57228U166894/specs?api_key=vppefiprYFtfGZV0M3vTxaIauROiVoVB',
+// return fetch(`https://marketcheck-prod.apigee.net/v1/search?api_key=${MARKETCHECKAPIKEY}&vins=${VINData}&latitude=33.7872303&longitude=-84.3826917&radius=200&car_type=used&start=0&rows=10`
+// https://marketcheck-prod.apigee.net/v1/history/1G4HP57228U166894?api_key=vppefiprYFtfGZV0M3vTxaIauROiVoVB
 
 export const getCarValueByVIN = (VINData) => {
     console.log(VINData);
@@ -65,38 +64,3 @@ export const getCarValueByVIN = (VINData) => {
     	}
     ).then((res) => res.json()).then(console.log);
 };
-
-
-// return fetch(`${MARKETCHECK}${MARKETCHECKAPIKEY}${VINPARAMS}${VINData}`, {
-    // Host: 'marketcheck-prod.apigee.net',
-    // 'Content-Type': 'application/json'
-    // 'Access-Control-Allow-Origin': '*','
-// headers: {
-//             'host': 'marketcheck-prod.apigee.net'
-//         }
-// return fetch('http://api.marketcheck.com/v1/search?api_key='+`${MARKETCHECKAPIKEY}`+'&vins='`${VINData}`+'&latitude=33.7872303&longitude=-84.3826917&radius=200&car_type=used&start=0&rows=10', {
-
-// export const getCarValueByVIN = (VINData) => {
-//     console.log(VINData);
-//     let settings = {
-// 		async: true,
-// 		crossDomain: true,
-// 		url: 'http://api.marketcheck.com/v1/vin/1G4HP57228U166894/specs?api_key=vppefiprYFtfGZV0M3vTxaIauROiVoVB',
-// 		method: 'GET',
-// 		headers: {
-// 			host: 'marketcheck-prod.apigee.net',
-// 			'Content-Type': 'application/json',
-// 			'cache-control': 'no-cache',
-// 			Origin: 'http://Ryans_MacBook_Pro:3001/vehicle_search_react_frontend',
-// 			'Access-Control-Request-Origin': 'http://Ryans_MacBook_Pro:3001/vehicle_search_react_frontend'
-// 		},
-// 		processData: false,
-// 		data: ''
-// 	};
-
-// 	$.ajax(settings).done(function(response) {
-// 		console.log(response);
-// 	});
-// };
-
-// https://marketcheck-prod.apigee.net/v1/history/1G4HP57228U166894?api_key=vppefiprYFtfGZV0M3vTxaIauROiVoVB
