4c9041aa28f9a7432abee4ea525504a4d3d74678
github username <github email address>
2019-04-30 11:21:57 -0400

    wrapped Router in a div with id

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-d
 class App extends Component {
     render() {
         return (
-            <Router>
-                <Switch>
-                    <Route exact path='/login' render={(props) => {return !localStorage.getItem('token') ? <Login {...props} /> : <Redirect to='/' /> }} />
-                    <Route exact path='/signup' render={(props) => {return !localStorage.getItem('token') ? <SignUp {...props} /> : <Redirect to='/' /> }} />
-                    <Route exact path='/' render={(props) => {return localStorage.getItem('token') ? <LandingSearchPage {...props} /> : <Redirect to='/login' /> }} />
-                    <Route exact path='/favorites' render={(props) => {return localStorage.getItem('token') ? <Favorites {...props} /> : <Redirect to='/login' /> }} />
-                </Switch>
-            </Router>
+            <div id='app-container'>
+                <Router>
+                    <Switch>
+                        <Route exact path='/login' render={(props) => {return !localStorage.getItem('token') ? <Login {...props} /> : <Redirect to='/' /> }} />
+                        <Route exact path='/signup' render={(props) => {return !localStorage.getItem('token') ? <SignUp {...props} /> : <Redirect to='/' /> }} />
+                        <Route exact path='/' render={(props) => {return localStorage.getItem('token') ? <LandingSearchPage {...props} /> : <Redirect to='/login' /> }} />
+                        <Route exact path='/favorites' render={(props) => {return localStorage.getItem('token') ? <Favorites {...props} /> : <Redirect to='/login' /> }} />
+                    </Switch>
+                </Router>
+            </div>
         );
     }
 }
