8aa5a6a2ee231bdd5f9529f6688180874ff7ba2d
github username <github email address>
2019-05-01 11:33:41 -0400

    added car card css stylesheet

--- src/LandingSearchPage/Components/CarForSaleCard.js
+++ src/LandingSearchPage/Components/CarForSaleCard.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Carousel } from 'react-responsive-carousel';
 
 import { Card, Grid, Image, Label, Button, Icon } from 'semantic-ui-react';
-import '../../App.css';
+import '../../Stylesheets/CarForSaleCard.css';
 
 
 const CarForSaleCard = (props) => {
