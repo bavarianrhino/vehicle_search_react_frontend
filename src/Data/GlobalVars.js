93a06f6f41440d33ccbca87a7b800ae93a4834c1
github username <github email address>
2019-05-04 00:04:07 -0400

    added link imports and cleaned up

--- src/Data/GlobalVars.js
+++ src/Data/GlobalVars.js
@@ -3,12 +3,12 @@
 export const LOCALHOST = 'http://localhost:3000';
 
 
-//=======API KEYS FROM .ENV=======//
+// ------------- API KEYS FROM .ENV ------------- //
 export const MCAPIKEY = process.env.REACT_APP_MARKETCHECK_API_KEY;
 console.log('API KEY', MCAPIKEY);
 
 
-//=======Market Check EndPoints=======//
+// ------------- Market Check EndPoints ------------- //
 export const COORS = 'https://cors-anywhere.herokuapp.com/';
 export const MCSALES = 'https://marketcheck-prod.apigee.net/v1/sales?api_key='
 export const MCSEARCH = 'https://marketcheck-prod.apigee.net/v1/search?api_key='
@@ -20,3 +20,7 @@ export const MODEL = '&model='
 export const TRIM = '&trim='
 export const ZIP = '&=zip'
 export const RADIUS = '&=radius'
+export const ROWS_10 = '&rows=10&start=1'
+export const ROWS_25 = '&rows=25&start=1'
+export const ROWS_50 = '&rows=50&start=1'
+export const PHOTOS = '&photo_links=true'
