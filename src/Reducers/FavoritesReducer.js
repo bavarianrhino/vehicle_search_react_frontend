3d753a5b5f684f38296a68155c89ac5931c01609
github username <github email address>
2019-05-08 12:28:30 -0400

    compare favs vin to assign car id for delete

--- src/Reducers/FavoritesReducer.js
+++ src/Reducers/FavoritesReducer.js
@@ -26,7 +26,11 @@ export const FavoritesReducer = (
 				if (state.api_urls.map(({ vin }) => vin).includes(action.json.vin) && state.favorites.length >= state.api_urls.length) {
 					return state;
 				} else {
-					return { ...state, favorites: [...state.favorites, action.json], loading: false };
+                    let car_id_obj = state.api_urls.find(({ vin }) => action.json.vin === vin)
+                    console.log(car_id_obj)
+                    let object2 = (({ ...action.json, car_id: car_id_obj.id }));
+                    console.log(object2);
+					return { ...state, favorites: [...state.favorites, object2], loading: false };
 				}
 
 			case 'LAND_FAVORITE_CAR':
